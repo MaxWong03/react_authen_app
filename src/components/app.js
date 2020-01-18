a32a9290e7c7ec2ece7e62dddf9a91ee0a5d933f
Max Wong <max@techsamurais.com>
2020-01-17 19:14:03 -0800

    rewrite to functional component

--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import Dashboard from './Dashboard';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className='app'>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path={'/'} component={Home}/>
-            <Route exact path={'/dashboard'} component={Dashboard}/>
-          </Switch>
-        </BrowserRouter>
-      </div>
-    );
-  }
+export default function App() {
+  const [auth, setAuth] = useState({
+    loggedInStatus: false,
+    user: {}
+  })
+  return (
+    <div className='app'>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path={'/'} component={Home} />
+          <Route exact path={'/dashboard'} component={Dashboard} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
