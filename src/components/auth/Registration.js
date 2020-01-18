8ea92c19235b1473b2b363c7ead2ec12845617ce
Max Wong <max@techsamurais.com>
2020-01-17 18:38:26 -0800

    finish registration component

--- src/components/auth/Registration.js
+++ src/components/auth/Registration.js
@@ -1,20 +1,22 @@
 import React, { useState } from 'react';
 
+
 const Registration = () => {
   const [info, setInfo] = useState({
     email: '',
     password: '',
-    password_confirmation: '',
+    passwordConfirmation: '',
     registrationErrors: ''
   })
 
   const onSubmit = event => {
-    console.log('form submitted');
     event.preventDefault();
+    console.log('info:', info);
   }
 
   const onChange = event => {
     const { name, value } = event.target;
+    console.log('name:', name, 'value:', value)
     setInfo(state => {
       return {
         ...state,
@@ -23,7 +25,7 @@ const Registration = () => {
     })
   }
 
-  const { email, password, password_confirmation, registrationErrors } = info;
+  const { email, password, passwordConfirmation, registrationErrors } = info;
 
   return (
     <div>
@@ -36,6 +38,25 @@ const Registration = () => {
           onChange={onChange}
           required
         />
+        <input
+          type='password'
+          name='password'
+          placeholder='Password'
+          value={password}
+          onChange={onChange}
+          required
+        />
+        <input
+          type='password'
+          name='passwordConfirmation'
+          placeholder='Password Confirmation'
+          value={passwordConfirmation}
+          onChange={onChange}
+          required
+        />
+        <button type="submit">
+          Register
+        </button>
       </form>
     </div>
   )
